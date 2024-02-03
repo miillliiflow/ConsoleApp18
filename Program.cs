5f380233e644855ac0d681eebdcba91bde9a0c8c
miillliiflow <am555333999@gmail.com>
2024-02-03 21:25:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Вывести в строку десять заглавных букв "а" через один пробел:
+
+            //char a = 'a';
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write(a + " ");
+            //}
+
+            //2.Вывести в обратном порядке числа от 50 до 40.
+
+            //for (int i = 50; i >= 40; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //3.Вывести в строку через пробел следующие натуральные числа: 6 12 24 30 36
+
+            //for (int i = 6; i <= 36; i += 6)
+            //{
+            //    Console.Write(i + " ");
+            //}
+
+            //4.Вывести двузначные числа, кратные 11, подсчитать их количество и сумму.
+            //int count = 0;
+            //int sum = 0;
+            //for (int i = 11; i <= 99; i += 11)
+            //{
+            //    Console.WriteLine(i + " ");
+            //    count++;
+            //    sum += i;
+            //}
+            //Console.WriteLine("Количество чисел: " + count);
+            //Console.WriteLine("Сумма чисел: " + sum);
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
